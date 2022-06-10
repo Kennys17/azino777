4e4fa29f8bd46652631e4a60d4caf53a766895e5
Кенесов Жасулан Талгатович <@lcoast.itstep>
2022-06-10 21:39:32 +0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = new express();
+app.post('/user',(request, response) =>{
+    console.log(request.body.userNumber)
+})
 
 app.use(express.static('static')).listen(5000, () =>{
     console.log('Express server listens on port 5000')
